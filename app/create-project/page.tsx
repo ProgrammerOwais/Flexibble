fade9e03f293a9a37b9603a9be50934e37cf2151
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2024-06-25 20:56:22 -0700

    commenting

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -2,7 +2,7 @@ import Modal from "@/components/Modal";
 import ProjectForm from "@/components/ProjectForm";
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
-
+// A component used to create a new project
 const CreateProject = async () => {
   const session = await getCurrentUser();
   if (!session?.user) redirect("/");
