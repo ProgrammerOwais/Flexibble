0e91f32650226dd698c3038a8c154245c00860cc
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2024-06-25 20:57:28 -0700

    testing

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -3,6 +3,7 @@ import ProjectForm from "@/components/ProjectForm";
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
 // A component used to create a new project
+// testing
 const CreateProject = async () => {
   const session = await getCurrentUser();
   if (!session?.user) redirect("/");
